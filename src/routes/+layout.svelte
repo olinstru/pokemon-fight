4304eb570e9f97f886eb242d37a13db0f787167e
Olivia LINSTRUMELLE <olinstru@gmail.com>
2024-01-11 13:18:21 +0100

    started project and added main pages

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,10 @@
+<menu>
+	<nav>
+		<a href="/home">Accueil</a>
+		<a href="/creation">Création de personnage</a>
+		<a href="/list">Liste des combattants</a>
+		<a href="/combat">Combat</a>
+	</nav>
+</menu>
+
+<slot />
---
+++ src/routes/creation/+page.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+	export let data;
+	import Pokemon from '$lib/components/Pokemon.svelte';
+</script>
+
+<h1>Création de personnage</h1>
+
+<form>
+	<div class="cards">
+		{#each data.pokemons as pokemon}
+			<Pokemon {pokemon} />
+		{/each}
+	</div>
+</form>
+
+<style>
+	.cards {
+		display: grid;
+		grid-template-columns: 1fr 1fr 1fr 1fr;
+		grid-gap: 16px;
+	}
+</style>

f5352a52f747572e7201679a2a28b9c8849f0bd0
Olivia 🦄 <olinstru@gmail.com>
2024-01-12 11:22:08 +0100

    image appears on pokemon select from dropdown

--- src/routes/creation/+page.svelte
+++ src/routes/creation/+page.svelte
@@ -1,21 +1,17 @@
 <script lang="ts">
 	import { enhance } from '$app/forms';
-	import Pokemon from '$lib/components/Pokemon.svelte';
 
-	let selectedId: number;
-	let selectedName = '';
+	let selectedId: number = 0;
+	let selectedName: string = '';
 
 	export let data;
-	const pokemon = data.pokemons;
+	const pokemons = data.pokemons;
 
-	function getPokemonInfo(pokemonId: number) {
-		return pokemon.find((pokemon) => pokemon.id === pokemonId);
-	}
+	$: selectedPokemon = pokemons.find((pokemon) => pokemon.id === selectedId);
 
 	const handleSelectChange = (event) => {
-		selectedId = event.target.value;
-		const pokemon = data.pokemons[selectedId - 1];
-		selectedName = pokemon.name;
+		selectedId = +event.target.value; // Convert to number
+		selectedName = pokemons[selectedId - 1]?.name || '';
 		console.log(selectedName, selectedId);
 	};
 </script>
@@ -26,22 +22,18 @@
 	<form method="POST">
 		<select name="pokemonId" on:change={handleSelectChange}>
 			<option value="">Choose a pokemon</option>
-			{#each data.pokemons as pokemon}
+			{#each pokemons as pokemon}
 				<option value={pokemon.id}>{pokemon.name}</option>
 			{/each}
 		</select>
-
 		<div>
 			<h3>Selected pokemon:</h3>
-			<!-- <img
-				src={getPokemonInfo(selectedPokemonId).sprites.front_default}
-				alt="Pokemon"
-				style="max-width: 100%; margin-bottom: 10px;"
-			/> -->
-			
-			<input type="text" name="pokemonName" placeholder="type here" value={selectedName} />
-			<button>Create</button>
+			{#if selectedPokemon}
+				<img src={selectedPokemon.sprites.front_default} alt={selectedPokemon.name} />
+			{/if}
 		</div>
+		<input type="text" name="pokemonName" placeholder="type here" value={selectedName} />
+		<button>Create</button>
 	</form>
 </div>
 
@@ -49,13 +41,8 @@
 	.container {
 		display: flex;
 	}
+
 	select {
 		width: 200px;
 	}
-
-	/* .pokemon-grid {
-		display: grid;
-		grid-template-columns: 1fr 1fr 1fr 1fr 1fr 1fr 1fr 1fr;
-		grid-gap: 20px;
-	} */
 </style>
